a25400f412f4e1d4f6fb9199fb8ec891f6ef7755
Patrick Hopman <patrickhopman@me.com>
2018-02-27 10:22:23 +0100

    Basic style

--- src/pages/alarms/alarms.ts
+++ src/pages/alarms/alarms.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-alarms',
+  templateUrl: 'alarms.html'
 })
-export class HomePage {
+export class AlarmsPage {
 
   constructor(public navCtrl: NavController) {
 
